d595b8af425000d0bfc10f851a1675d26b9a3060
BertaVR <bverges@cifpfbmoll.eu>
2021-03-16 19:27:56 +0100

    feat:  Getters y setters de furnace

--- furnace/src/main/java/edu/pingpong/RoomTemperature.java
+++ furnace/src/main/java/edu/pingpong/RoomTemperature.java
@@ -1,5 +1,19 @@
 package edu.pingpong;
 
 public class RoomTemperature {
+
+
     private double temperature = 0;
+
+    public RoomTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
